9ba1a30f6628f2f018033a6350a4ee3fd87e4d69
Ambrose Chua <ambrose@chua.family>
2018-04-01 18:19:48 +0800

    Better caddy and neovim versioning

--- .install/caddy.sh
+++ .install/caddy.sh
@@ -7,8 +7,13 @@ echo
 echo "Installing Caddy $CADDY_VERSION..."
 echo
 
-git clone https://github.com/mholt/caddy -b "v$CADDY_VERSION" $(go env GOPATH)/src/github.com/mholt/caddy
+git clone https://github.com/mholt/caddy $(go env GOPATH)/src/github.com/mholt/caddy
 cd $(go env GOPATH)/src/github.com/mholt/caddy/caddy
+if [ "$CADDY_VERSION" = "master" ]; then
+	git checkout $CADDY_VERSION
+else
+	git checkout v$CADDY_VERSION
+fi
 go get github.com/caddyserver/builds
 go run build.go
 go install
--- .install/neovim.sh
+++ .install/neovim.sh
@@ -20,11 +20,15 @@ apk add --no-cache --virtual .build-deps \
 
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v$NEOVIM_VERSION
-
-# temp fix for #8056 until next release
-sed -i 's/https://raw.githubusercontent.com/mpeterv/luacheck/master/luacheck-scm-1.rockspec/https://luarocks.org/manifests/mpeterv/luacheck-0.21.2-1.rockspec/g' third-party/cmake/BuildLuarocks.cmake
-
+if [ "$NEOVIM_VERSION" = "nightly" ]; then
+	git checkout $NEOVIM_VERSION
+else
+	git checkout v$NEOVIM_VERSION
+fi
+if [ "$NEOVIM_VERSION" = "0.2.2" ]; then
+	# temp fix for #8056 until next release
+	sed -i 's/https:\/\/raw\.githubusercontent\.com\/mpeterv\/luacheck\/master\/luacheck-scm-1\.rockspec/https:\/\/luarocks\.org\/manifests\/mpeterv\/luacheck-0\.21\.2-1\.rockspec/g' third-party/cmake/BuildLuarocks.cmake
+fi
 make
 make install
 cd ..
