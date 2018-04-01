ed05d9c023623991b7136c0d03ea191372698457
Ambrose Chua <ambrose@chua.family>
2018-04-01 23:53:43 +0800

    Ugh

--- .install/plug.sh
+++ .install/plug.sh
@@ -10,4 +10,6 @@ echo
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim +PlugInstall +qa
+set +e
+
+nvim +PlugInstall! +qa
