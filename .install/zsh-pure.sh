aab4af94683f7940a305d46d99d7552b7e356f9c
Ambrose Chua <ambrose@chua.family>
2018-04-02 14:55:26 +0800

    Set proper folder perms

--- .install/zsh-pure.sh
+++ .install/zsh-pure.sh
@@ -7,8 +7,9 @@ echo
 echo "Installing pure-prompt for ZSH..."
 echo
 
-curl -fLo /usr/local/share/zsh/site-functions/prompt_pure_setup --create-dirs \
+mkdir /usr/local/share/zsh/site-functions/
+curl -fLo /usr/local/share/zsh/site-functions/prompt_pure_setup \
 	https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh
-curl -fLo /usr/local/share/zsh/site-functions/async --create-dirs \
+curl -fLo /usr/local/share/zsh/site-functions/async \
 	https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh
 
