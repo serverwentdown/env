a0135423b0ecba9099daafa89789bfbc93a8f696
Ambrose Chua <ambrose@chua.family>
2018-04-02 03:08:50 +0000

    Update zsh-pure to manual install

--- .install/zsh-pure.sh
+++ .install/zsh-pure.sh
@@ -7,4 +7,12 @@ echo
 echo "Installing pure-prompt for ZSH..."
 echo
 
-yarn global add pure-prompt
+mkdir ~/.zfunctions
+curl -fLo ~/.zfunctions/prompt_pure_setup https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh
+curl -fLo ~/.zfunctions/async https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh
+
+echo '# pure' >> ~/.zshrc
+echo 'fpath=( "$HOME/.zfunctions" $fpath )' >> ~/.zshrc
+echo 'autoload -U promptinit; promptinit' >> ~/.zshrc
+echo 'prompt pure' >> ~/.zshrc
+
