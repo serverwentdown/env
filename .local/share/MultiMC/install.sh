ec709032c0bb05264874cc0935616ff88d4fe491
Ambrose Chua <ambrose@hey.com>
2024-11-25 23:18:08 +0800

    Clean up unused sway configuration

--- .local/share/MultiMC/install.sh
+++ .local/share/MultiMC/install.sh
@@ -5,29 +5,26 @@ set -euo pipefail
 echo "Installing MultiMC"
 
 INSTALL_LOCATION="$HOME/.local/share/MultiMC"
-DOWNLOAD_URL="https://files.multimc.org/downloads/mmc-stable-lin64.tar.gz"
-LOGO_URL="https://avatars2.githubusercontent.com/u/5411890"
+DOWNLOAD_URL="https://files.multimc.org/downloads/mmc-develop-lin64.tar.gz"
 
 # Paths
 mkdir -p "$INSTALL_LOCATION"
 
 # Package
 wget --output-document /tmp/multimc.tar.gz "$DOWNLOAD_URL"
-tar --extract --strip-components 1 --file /tmp/multimc.tar.gz --directory "$INSTALL_LOCATION"
-
-# Logo
-wget --output-document $HOME/.local/share/icons/multimc.png "$LOGO_URL"
+tar --verbose --extract --strip-components 1 --file /tmp/multimc.tar.gz --directory "$INSTALL_LOCATION"
 
 # Desktop Entry
-cat > $HOME/.local/share/applications/multimc.desktop <<EOF
+cat > $HOME/.local/share/applications/MultiMC.desktop <<EOF
 [Desktop Entry]
 Type=Application
 Name=MultiMC
 Categories=Game;
-X-GNOME-FullName=MultiMC
-Comment=MultiMC Minecraft Launcher
-Icon=multimc
-Exec=$INSTALL_LOCATION/MultiMC
+Exec=$INSTALL_LOCATION/MultiMC -d $INSTALL_LOCATION
+StartupNotify=true
+Terminal=false
+Path=$INSTALL_LOCATION
+Icon=$INSTALL_LOCATION/icons/shortcut-icon.png
 EOF
 
 # Cleanup
